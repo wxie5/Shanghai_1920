3f99788b14126dbaaf979cab65d3c1c9e0fee487
Brian <bjjx1999@live.com>
2021-04-20 19:45:00 +0800

    Add simple scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    public float jumpForce;
+    public Transform bottom;
+    public LayerMask groundLayer;
+    
+    private Rigidbody2D body;
+    private bool onGround;
+
+    private void Start() {
+        body = GetComponent<Rigidbody2D>();
+        // rigidbody.MovePosition(new Vector2(6.9f, 1.2f));
+    }
+
+    private void FixedUpdate() {
+        var xDir = Input.GetAxis("Horizontal") * speed;
+        var dir = new Vector2(xDir, body.velocity.y);
+        body.velocity = dir;
+        
+        var jump = Input.GetButtonDown("Jump");
+        if (jump) {
+            var hit = Physics2D.Raycast(bottom.position, Vector2.down, 0.1f, groundLayer);
+            if (hit) {
+                body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Pickup")) {
+            print("Item pickup");
+            Destroy(other.gameObject);
+        }
+    }
+}
